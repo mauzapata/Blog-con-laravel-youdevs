9e708b088005c0c189e872d058cf9c710a772c40
mau.zapata95 <mau.zapata95@gmail.com>
2022-02-23 09:20:30 -0500

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Homepage - Post</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css" 
+</head>
+
+<body>
+    <!-- Logo -->
+    <nav class="navbar navbar-light bg-main">
+        <div class="container p-4">
+            <a class="navbar-brand m-auto" href="#">
+                <img src="{{asset('images/logo.png')}}" width="200" alt="" loading="lazy">
+            </a>
+        </div>
+    </nav>
+
+
+    <!-- Contenido -->
+    <section class="container-fluid content">
+        <!-- Categorías -->
+        <div class="row justify-content-center">
+            <div class="col-10 col-md-12">
+                <nav class="text-center my-5">
+                    <a href="#" class="mx-3 pb-3 link-category d-block d-md-inline selected-category">Todas</a>
+                    <a href="#" class="mx-3 pb-3 link-category d-block d-md-inline">Programación</a>
+                    <a href="#" class="mx-3 pb-3 link-category d-block d-md-inline">Desarrollo web</a>
+                </nav>
+            </div>
+        </div>
+
+        <!-- Posts -->
+        <div class="row justify-content-center">
+            <div class="col-10">
+                <div class="row">
+                    <!-- Post 1 -->
+                    <div class="col-md-4 col-12 justify-content-center mb-5">
+                        <div class="card m-auto" style="width: 18rem;">
+                            <img class="card-img-top" src="{{asset('images/3.png')}}" alt="Post Python">
+                            <div class="card-body">
+                                <small class="card-txt-category">Categoría: Programación</small>
+                                <h5 class="card-title my-2">Aprende Python en un dos tres</h5>
+                                <div class="d-card-text">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Sed voluptatum ab cumque quisquam quod nesciunt fugiat,
+                                    eius corrupti nulla veniam, molestias nemo repudiandae?
+                                </div>
+                                <a href="#" class="post-link"><b>Leer más</b></a>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-6 text-left">
+                                        <span class="card-txt-author">YouDevs</span>
+                                    </div>
+                                    <div class="col-6 text-right">
+                                        <span class="card-txt-date">Hace 2 semanas</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Post 2 -->
+                    <div class="col-md-4 col-12 justify-content-center mb-5">
+                        <div class="card m-auto" style="width: 18rem;">
+                            <img class="card-img-top" src="{{asset('images/4.png')}}" alt="Post Python">
+                            <div class="card-body">
+                                <small class="card-txt-category">Categoría: Programación</small>
+                                <h5 class="card-title my-2">Aprende Python en un dos tres</h5>
+                                <div class="d-card-text">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Sed voluptatum ab cumque quisquam quod nesciunt fugiat,
+                                    eius corrupti nulla veniam, molestias nemo repudiandae?
+                                </div>
+                                <a href="#" class="post-link"><b>Leer más</b></a>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-6 text-left">
+                                        <span class="card-txt-author">YouDevs</span>
+                                    </div>
+                                    <div class="col-6 text-right">
+                                        <span class="card-txt-date">Hace 2 semanas</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Post 3 -->
+                    <div class="col-md-4 col-12 justify-content-center mb-5">
+                        <div class="card m-auto" style="width: 18rem;">
+                            <img class="card-img-top" src="{{asset('images/5.png')}}" alt="Post Python">
+                            <div class="card-body">
+                                <small class="card-txt-category">Categoría: Programación</small>
+                                <h5 class="card-title my-2">Aprende Python en un dos tres</h5>
+                                <div class="d-card-text">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Sed voluptatum ab cumque quisquam quod nesciunt fugiat,
+                                    eius corrupti nulla veniam, molestias nemo repudiandae?
+                                </div>
+                                <a href="#" class="post-link"><b>Leer más</b></a>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-6 text-left">
+                                        <span class="card-txt-author">YouDevs</span>
+                                    </div>
+                                    <div class="col-6 text-right">
+                                        <span class="card-txt-date">Hace 2 semanas</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12">
+                <!-- Paginador -->
+
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="container-fluid bg-main">
+        <div class="row text-center p-4">
+            <div class="mb-3">
+                <img src="{{asset('images/logo.png')}}" alt="YouDevs logo" width="120" id="logofooter">
+            </div>
+            <div id="col-md-10">
+                <a href="https://www.facebook.com/youdevs">
+                    <img src="{{asset('images/facebook.png')}}" class="img-fluid" width="40px" alt="facebook youdevs">
+                </a>
+                <a href="https://www.instagram.com/youdevs">
+                    <img src="{{asset('images/instagram.png')}}" class="img-fluid" width="40px" alt="instagram youdevs">
+                </a>
+                <a href="https://www.youtube.com/c/YouDevsOficial">
+                    <img src="{{asset('images/youtube.png')}}" class="img-fluid" width="40px" alt="youtube youdevs">
+                </a>
+                <p class="mt-3">Copyright © 2020 YouDevs, Blog. <br> Todos los derechos reservados.</p>
+            </div>
+        </div>
+    </footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
